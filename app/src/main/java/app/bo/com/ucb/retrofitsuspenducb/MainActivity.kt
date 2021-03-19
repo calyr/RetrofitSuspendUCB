f98384ff1b187c8a5b9e8fc2bb59beb3ff458788
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-03-19 18:53:20 -0400

    added suspend function in Retrofit

--- app/src/main/java/app/bo/com/ucb/retrofitsuspenducb/MainActivity.kt
+++ app/src/main/java/app/bo/com/ucb/retrofitsuspenducb/MainActivity.kt
@@ -2,10 +2,23 @@ package app.bo.com.ucb.retrofitsuspenducb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restApiAdapter = RestApiAdapter()
+        val endPoint = restApiAdapter.conectionApi()
+        GlobalScope.launch(Dispatchers.IO) {
+            val posts = endPoint.getAllPost()
+            posts.forEach {
+                Log.d("post", it.title)
+            }
+        }
     }
 }
